87d4096245e24384128730f1738d854e10ccf8b7
SagMCU <52503815+SagMCU@users.noreply.github.com>
2025-02-12 14:26:09 +0100

    Added Bstats manually

--- PistonLimits/src/main/java/com/sagmcu/PistonLimits/Main.java
+++ PistonLimits/src/main/java/com/sagmcu/PistonLimits/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.sagmcu.PistonLimits.commands.ReloadCommand;
 import com.sagmcu.PistonLimits.mechanisms.PistonLimiter;
 import com.sagmcu.PistonLimits.utils.ChatColorTranslator;
+import com.sagmcu.PistonLimits.bstats.Metrics;
 
 public class Main extends JavaPlugin {
     private PistonLimiter pistonLimiter;
@@ -15,6 +16,9 @@ public class Main extends JavaPlugin {
         pistonLimiter = new PistonLimiter(this);
         getServer().getPluginManager().registerEvents(pistonLimiter, this);
         getCommand("pistonlimits").setExecutor(new ReloadCommand(this));
+        int pluginId = 24759;
+        @SuppressWarnings("unused")
+        Metrics metrics = new Metrics(this, pluginId);
         String enabledMessage = ChatColorTranslator.translate("&aPistonLimits &7has been &aenabled&7! &6:D");
         this.getServer().getConsoleSender().sendMessage(enabledMessage);
     }
